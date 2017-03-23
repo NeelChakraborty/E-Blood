f7fb35b499fde18a4e996c37f942874f8e411abb
Souradip Chakraborty <chakraborty.souradip@gmail.com>
2017-03-23 21:44:28 +0530

    Rejected commit: Default

--- app/src/main/java/com/example/android/e_blood/UserLoginActivity.java
+++ app/src/main/java/com/example/android/e_blood/UserLoginActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.e_blood;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -16,12 +23,17 @@ public class UserLoginActivity extends AppCompatActivity {
     private static final String TAG = "Registration";
     private EditText emailEditText;
     private EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
-        emailEditText = (EditText) findViewById(R.id.email_sign_in);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEditText = (EditText) findViewById(R.id.email_sign_in);
+        passwordEditText = (EditText) findViewById(R.id.password_sign_in);
+        View signInButton = (View) findViewById(R.id.sign_in);
 
         TextView registerTextView = (TextView) findViewById(R.id.register);
         registerTextView.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +43,65 @@ public class UserLoginActivity extends AppCompatActivity {
                 startActivity(userRegistrationIntent);
             }
         });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                signInUser();
+            }
+        });
+    }
+
+    private void signInUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(UserLoginActivity.this, "Wrong username or password",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- app/src/main/java/com/example/android/e_blood/UserRegistrationActivity.java
+++ app/src/main/java/com/example/android/e_blood/UserRegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.e_blood;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,9 +27,8 @@ public class UserRegistrationActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "Registration";
-    private EditText emailEditText;
-    private EditText passwordEditText;
-
+    private EditText emailEditText, passwordEditText, nameEditText, phoneEditText, dobEditText, addressEditText, occupationEditText;
+    private Spinner bloodgroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +41,30 @@ public class UserRegistrationActivity extends AppCompatActivity {
         //initializing Views
         emailEditText = (EditText) findViewById(R.id.email_edit_text);
         passwordEditText = (EditText) findViewById(R.id.password_edit_text);
-
+        nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        phoneEditText = (EditText) findViewById(R.id.phone_edit_text);
+        dobEditText = (EditText) findViewById(R.id.DOB_edit_text);
+        addressEditText = (EditText) findViewById(R.id.address_edit_text);
+        occupationEditText = (EditText) findViewById(R.id.occupation_edit_text);
+        bloodgroup = (Spinner) findViewById(R.id.blood_group_spinner);
         View registerButton = (View) findViewById(R.id.register_button);
+
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.blood_group, R.layout.spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bloodgroup.setAdapter(adapter);
+        bloodgroup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String userBloodGroup = bloodgroup.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -84,6 +109,9 @@ public class UserRegistrationActivity extends AppCompatActivity {
                         // ...
                     }
                 });
+    }
+
+    private void addDetails() {
     }
 
     @Override
